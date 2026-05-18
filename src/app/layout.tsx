89484ddc6fc436c3ff0627d43ca1096840468c8a
didepanlayar <albinmaulana@yahoo.com>
2026-05-18 11:37:27 +0700

    Feature: Setup modal router

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -18,7 +20,10 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        {modal}
+      </body>
     </html>
   );
 }
